3ec09de9c2ecd32efc21c89b953e7a7c88ae2190
John O. Mark <hackcraftblog@gmail.com>
2025-10-19 14:14:47 +0300

    I removed flask-migrate imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from config import Config
 
 # Initialize extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
-migrate = Migrate()
+# migrate = Migrate()
 
 def create_app(config_class=Config):
     """Application factory function"""
@@ -17,7 +17,7 @@ def create_app(config_class=Config):
     # Initialize Flask extensions
     db.init_app(app)
     login_manager.init_app(app)
-    migrate.init_app(app, db)
+    # migrate.init_app(app, db)
     
     # Configure login manager
     login_manager.login_view = 'login'
